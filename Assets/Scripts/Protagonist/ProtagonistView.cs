3fb09941cd4f5517ee7295b467953c28d163a336
tonycool <dlords777@gmail.com>
2022-03-10 23:55:12 +0200

    converted all gameobject to ui elements

--- Assets/Scripts/Protagonist/ProtagonistView.cs
+++ Assets/Scripts/Protagonist/ProtagonistView.cs
@@ -1,41 +1,41 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ProtagonistView : View
 {
-    public float MovementSpeed = 10f;
+    public float MovementSpeed = 3f;
     public Animator ProtagonistAnimator;
-    public SpriteRenderer ProtagonistRenderer;
     private float _Direction;
     private bool Moving = false;
     Vector2 LastClickedPosition;
     float Speed => Time.deltaTime * MovementSpeed;
-
 
     private void Update()
     {
         if (Input.GetMouseButtonDown(0))
         {
             LastClickedPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            Moving = true;
+            Moving = !!Input.GetMouseButtonDown(0);
         }
 
         if (Moving && (Vector2)transform.position != LastClickedPosition)
         {
             _Direction = transform.position.x > LastClickedPosition.x ? 1f : -1f;
             transform.position = Vector2.MoveTowards(transform.position, LastClickedPosition, Speed);
+            SetAnimation(_Direction);
         }
         else
         {
             _Direction = 0;
+            SetAnimation(_Direction);
         }
-        SetAnimation(_Direction);
     }
 
     public void SetAnimation(float direction)
     {
         ProtagonistAnimator.SetFloat("Horizontal", direction);
-        ProtagonistRenderer.flipX = (direction > 0f);
+        transform.rotation = (direction > 0f) ? Quaternion.AngleAxis(-180, Vector3.up) : Quaternion.AngleAxis(0, Vector3.up);
     } 
 }
