0ab39256e916138dcc4a1530c97d5c1ada27c2e6
tonycool <dlords777@gmail.com>
2022-03-16 13:28:12 +0200

    started dialogue system

--- Assets/Scripts/Monsters/Generics/AbstractMonsterModel.cs
+++ Assets/Scripts/Monsters/Generics/AbstractMonsterModel.cs
@@ -1,11 +1,12 @@
 using System;
+using System.Collections;
 using UnityEngine;
 
 public abstract class AbstractMonsterModel : MonoBehaviour, IMonstersModel
 {
     protected Vector2 _MonsterPosition;
     protected float _MonsterHealth = 100;
-    protected Enum _MonsterDialogues;
+    protected Queue _MonsterDialogues;
     protected bool _Aggressor = false;
     protected string _MonsterName;
     virtual public Vector2 MonsterPosition { get { return _MonsterPosition; } set { _MonsterPosition = value; } }
@@ -13,7 +14,7 @@ public abstract class AbstractMonsterModel : MonoBehaviour, IMonstersModel
         get { return _MonsterHealth; } 
         set { _MonsterHealth += ((_MonsterHealth + value) >= value) ? value : 0; } 
     }
-    virtual public Enum MonsterDialogues { get { return _MonsterDialogues; } set { _MonsterDialogues = value; } }
+    virtual public Queue MonsterDialogues { get { return _MonsterDialogues; } set { _MonsterDialogues = value; } }
     virtual public bool Aggressor { get { return _Aggressor; } set { _Aggressor = value; } }
     virtual public string MonsterName { get { return _MonsterName; } set { _MonsterName = value; } }
 }
--- Assets/Scripts/Monsters/Interfaces/IMonstersModel.cs
+++ Assets/Scripts/Monsters/Interfaces/IMonstersModel.cs
@@ -1,11 +1,12 @@
 using System;
+using System.Collections;
 using UnityEngine;
 
 public interface IMonstersModel
 {
     public Vector2 MonsterPosition { get; set; }
     public float MonsterHealth { get; set; }
-    public Enum MonsterDialogues { get; set; }
+    public Queue MonsterDialogues { get; set; }
     public bool Aggressor { get; set; }
 
     public string MonsterName { get; set; }
